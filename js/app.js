46088eabdc6cda524dada014234f0048ac9ee297
Tharles Moreto <contatotharlesmoreto@gmail.com>
2023-06-29 17:35:58 -0300

    novas alterações

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
+import {perguntas, respostas} from './perguntaserespostas.js';
 
 function viewProcess (){
   let process = document.querySelector('.process');
   process.classList.add("show");
 
-  let viewStart = document.querySelector('.start');
-  viewStart.classList.add("show");
+  let start = document.querySelector('.start');
+  start.classList.add("show");
+
  
 }
 
-let btnStart = document.querySelector('.btn');
+let btnStart = document.querySelector('.btnStart');
 btnStart.addEventListener("click", viewProcess);
 
 
+let indice = 0
+
+function next (){
+
+  if (indice < perguntas.length){
+    let pergunta = document.querySelector('.perguntaProcess')
+    pergunta.textContent = perguntas[indice]
+    indice++
+  }
+}
+
+
+let nextProcess = document.querySelector('.btnProcess');
+nextProcess.addEventListener("click", next);
